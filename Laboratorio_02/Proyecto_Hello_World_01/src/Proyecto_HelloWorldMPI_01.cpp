2b4b6ba19c73b54bff1c47e90ab1d64a4f4e9dd2
JUANFT11 <jfernandeztuni@gmail.com>
2020-01-24 23:37:37 -0500

    Hola Mundo

---
+++ Laboratorio_02/Proyecto_Hello_World_01/src/Proyecto_HelloWorldMPI_01.cpp
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Proyecto_HelloWorldMPI_01.c
+ Author      : Juan Fernandez
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Compute Pi in MPI C++
+ ============================================================================
+ */
+#include <iostream>
+using namespace std;
+ 
+int main(int argc, char *argv[])
+{
+    cout<<"¡Hola Mundo soy proceso unico! "<<endl;
+
+    return 0;
+}
+
+
